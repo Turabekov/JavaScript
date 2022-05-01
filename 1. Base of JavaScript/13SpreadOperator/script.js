5878dde1deacf6cda44ed4cbe53c232fb90021fd
Khumoyun <turabekov1110@gmail.com>
2022-05-01 18:45:16 +0500

    Spread operator

---
+++ 1. Base of JavaScript/13SpreadOperator/script.js
@@ -0,0 +1,113 @@
+"use strcit";
+
+let a = 5,
+    b = a; // b = 5
+
+b = b + 5;
+
+console.log(b); // 10
+console.log(a); // 5
+//==========================
+
+const obj = {
+    a: 5,
+    b: 1,
+};
+
+const copy = obj; // ! link to object [obj]
+
+copy.a = 10;
+
+console.log(copy);
+console.log(obj);
+
+//===========================================
+
+function copyObj(mainObj) {
+    let objCopy = {};
+
+    let key;
+
+    for (key in mainObj) {
+        objCopy[key] = mainObj[key];
+    }
+
+    return objCopy;
+}
+
+const numbers = {
+    a: 2,
+    b: 5,
+    c: {
+        x: 7,
+        y: 4,
+    },
+};
+
+const newNumbers = copyObj(numbers);
+
+newNumbers.a = 10;
+newNumbers.c.x = 10;
+
+console.log(newNumbers);
+console.log(numbers);
+
+//==================================================
+
+const add = {
+    d: 17,
+    e: 20,
+};
+
+// console.log(Object.assign(numbers, add));
+const clone = Object.assign({}, add);
+
+clone.d = 20;
+
+console.log(add);
+console.log(clone);
+
+//=====================================================
+
+const oldArray = ["a", "b", "c"];
+const newArray = oldArray.slice();
+
+newArray[1] = "dsdsd";
+console.log(newArray);
+console.log(oldArray);
+
+//==Spread operator================================================
+
+const video = ["youtube", "vimeo", "rutube"],
+    blogs = ["wordpress", "livejournal", "blogger"],
+    internet = [...video, ...blogs, "vk", "facebook"];
+
+console.log(internet);
+
+function log(a, b, c) {
+    console.log(a);
+    console.log(b);
+    console.log(c);
+}
+
+const num = [2, 5, 7];
+
+log(...num);
+
+//=========================================================================
+
+const array = ["a", "b"];
+
+const newArray = [...array];
+
+const q = {
+    one: 1,
+    two: 2,
+};
+
+const newObj = { ...q };
+
+newObj.one = 4;
+
+console.log(newObj);
+console.log(q);
