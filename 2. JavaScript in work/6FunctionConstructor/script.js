4442139eb765f2c5b0c81bb8f312b8018183ace8
Khumoyun <turabekov1110@gmail.com>
2022-05-12 16:24:37 +0500

    Function Constructor

---
+++ 2. JavaScript in work/6FunctionConstructor/script.js
@@ -0,0 +1,29 @@
+"use strict";
+
+// const num = new Function(3);
+// console.log(num);
+
+// ! constructor
+function User(name, id) {
+    this.name = name;
+    this.id = id;
+    this.human = true;
+    this.hello = function () {
+        console.log(`Hello ${this.name}`);
+    };
+}
+
+User.prototype.exit = function () {
+    console.log(`User ${this.name} left`);
+};
+
+const ivan = new User("Ivan", 28);
+const alex = new User("Alex", 20);
+
+ivan.exit();
+
+ivan.hello();
+alex.hello();
+
+console.log(ivan);
+console.log(alex);
