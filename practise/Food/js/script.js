610e311610c66832985582fad2f294f1fac8412d
Khumoyun <turabekov1110@gmail.com>
2022-05-17 19:04:06 +0500

    practise XMLHttpRequest and Promise

--- practise/Food/js/script.js
+++ practise/Food/js/script.js
@@ -106,8 +106,7 @@ window.addEventListener("DOMContentLoaded", () => {
     // Modal
 
     const modalTrigger = document.querySelectorAll("[data-modal]"),
-        modal = document.querySelector(".modal"),
-        modalCloseBtn = document.querySelector("[data-close]");
+        modal = document.querySelector(".modal");
 
     modalTrigger.forEach((btn) => {
         btn.addEventListener("click", openModal);
@@ -126,10 +125,8 @@ window.addEventListener("DOMContentLoaded", () => {
         document.body.style.overflow = "";
     }
 
-    modalCloseBtn.addEventListener("click", closeModal);
-
     modal.addEventListener("click", (e) => {
-        if (e.target === modal) {
+        if (e.target === modal || e.target.getAttribute("data-close") == "") {
             closeModal();
         }
     });
@@ -231,6 +228,90 @@ window.addEventListener("DOMContentLoaded", () => {
         ".menu .container",
         "menu__item"
     ).render();
+
+    // ? ==============================================================================================
+    // Form
+
+    const forms = document.querySelectorAll("form");
+
+    forms.forEach((item) => {
+        postData(item);
+    });
+
+    const message = {
+        loading: "img/form/spinner.svg",
+        succes: "Спасибо! Скоро мы с вами свяжемся",
+        failure: "Что-то пошло не так...",
+    };
+
+    function postData(form) {
+        form.addEventListener("submit", (e) => {
+            e.preventDefault();
+
+            const statusMessage = document.createElement("img");
+            statusMessage.src = message.loading;
+            statusMessage.style.cssText = `
+                display: block;
+                margin: 0 auto;
+            `;
+            // form.append(statusMessage);
+            form.insertAdjacentElement("afterend", statusMessage);
+
+            const request = new XMLHttpRequest();
+
+            request.open("POST", "server.php");
+
+            // ! when we use XMLHttpRequest + FormData we must not use setRequestHeader
+            // request.setRequestHeader("Content-type", "multipart/form-data");
+
+            request.setRequestHeader("Content-type", "aplication/json");
+            const formData = new FormData(form);
+
+            const object = {};
+            formData.forEach(function (value, key) {
+                object[key] = value;
+            });
+
+            const json = JSON.stringify(object);
+
+            request.send(json);
+
+            request.addEventListener("load", () => {
+                if (request.status === 200) {
+                    console.log(request.response);
+                    showThanksModal(message.succes);
+                    form.reset();
+                    statusMessage.remove();
+                } else {
+                    showThanksModal(message.failure);
+                }
+            });
+        });
+    }
+
+    function showThanksModal(message) {
+        const prevModalDialog = document.querySelector(".modal__dialog");
+
+        prevModalDialog.classList.add("hide");
+        openModal();
+
+        const thanksModal = document.createElement("div");
+        thanksModal.classList.add("modal__dialog");
+        thanksModal.innerHTML = `
+            <div class="modal__content">
+                <div class="modal__close" data-close>×</div>
+                <div class="modal__title">${message}</div>
+            </div>
+        `;
+
+        document.querySelector(".modal").append(thanksModal);
+        setTimeout(() => {
+            thanksModal.remove();
+            prevModalDialog.classList.add("show");
+            prevModalDialog.classList.remove("hide");
+            closeModal();
+        }, 3000);
+    }
 
     // ? ==============================================================================================
 });
